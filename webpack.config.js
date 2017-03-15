2f8acd867f745eff939fa414482fc879d5c44fef
kcy0142 <kcy0142@naver.com>
2017-03-15 13:30:22 +0900

    1차 수정

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ const entryTarget = glob.sync('./src/app/*/*.ts')
        path: path.resolve(__dirname, './dist'),
        publicPath: '/',
         filename: '[name].[chunkhash].js'
-     },
+     }, 
      // output: {
      //     path: path.resolve(__dirname, './dist'),
      //     filename: 'app.bundle.js'
@@ -50,17 +50,90 @@ const entryTarget = glob.sync('./src/app/*/*.ts')
              path.resolve(__dirname, './src'),
              {}
          ),
+         new webpack.optimize.CommonsChunkPlugin(
+          {
+            name:['vendor','manifest']
+          }),
          new HtmlWebpackPlugin({
              template: './src/index.html'
          })
      ],
-     module: {
-         loaders: [
+     module:{
+          rules:[
+            {
+              use:'babel-loader',
+              test:/\.js$/,
+              exclude:/node_modules/
+            },
+            {
+              use:['style-loader','css-loader'],
+              test:/\.css$/
+            },
              { test: /\.ts$/, loaders: ['awesome-typescript-loader']}
-         ]
-     },
+          ]
+        },
+
+    //  module: {
+    //      loaders: [
+    //          { test: /\.ts$/, loaders: ['awesome-typescript-loader']}
+    //      ]
+    //  },
      resolve: {
          extensions: ['.ts', '.js'],
          modules: [ path.resolve(__dirname, 'node_modules') ]
      }
  };
+
+
+ //  new HtmlWebpackPlugin({
+ //      template: './src/index.html'
+ //  })
+//const entryTarget = glob.sync('./src/app/*/*.ts')
+ var pages = getEntry("./src/**/*.html");
+ var pages1 = getEntry("./src/app/*/*.html");
+for (var pathname in pages) {
+  //
+  var conf = {
+    filename: './src/' + pathname + '.html', //  export the path of html files
+    template: pages[pathname], //
+    inject: true,              //
+    minify: {
+      removeComments: true,
+      collapseWhitespace: false
+    }
+  };
+  if (pathname in module.exports.entry) {
+    conf.chunks = ['vendors', pathname];
+    conf.hash = false;
+  }
+
+  console.log('confconfconfconfconfconf 1==> ', conf)
+  // how many html files to be generated, we config  the same quatity of HtmlWebpackPlugin object
+  //03.15일단 주석처리하자.
+  //module.exports.plugins.push(new HtmlWebpackPlugin(conf));
+}
+
+function getEntry(globPath) {
+  var entries = {},
+    basename, tmp, pathname;
+
+  glob.sync(globPath).forEach(function (entry) {
+    basename = path.basename(entry, path.extname(entry));
+	var tmpWholeArray = entry.split('/');
+	if(tmpWholeArray.length >= 2 && path.extname(entry) == '.js'){
+		var libFolder = tmpWholeArray.splice(-2,1);
+		if(libFolder != "lib"){
+			 var tmp = entry.split('/');
+			 var tmp1 = entry.split('/').splice(2, tmp.length-3);
+			 pathname = tmp1.join("/") + '/' + basename;   // export js and html's path correctly
+			 entries[pathname] = entry;
+		}
+	}else{
+		var tmp = entry.split('/');
+		var tmp1 = entry.split('/').splice(2, tmp.length-3);
+		pathname = tmp1.join("/") + '/' + basename;    // export js and html's path correctly
+		entries[pathname] = entry;
+	}
+  });
+  return entries;
+}
